66198bf411aab5490d062a6990ce36f1f73270a3
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-02-21 23:25:47 +0100

    Update friends.jsx

--- src/components/friends/friends.jsx
+++ src/components/friends/friends.jsx
@@ -6,7 +6,7 @@ export const Friends = ({ item:{name, avatar, isOnline} }) => {
   return (
     <div className={css.friend_item}>
       
-      <img src={avatar} width='50' />
+      <img src={avatar} alt="user" width='50' />
       <span className={css.name_status}>{name}<p className={
       isOnline ? css.isOnline : css.isOffline
       }></p></span>
